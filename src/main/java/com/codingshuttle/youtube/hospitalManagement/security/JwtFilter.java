8fab35b000407998ac3ff08bec342c862516e94e
abhinav <abhinavsingh153@gmail.com>
2026-03-03 15:54:54 +0530

    add HandlerExceptionResolver
    - pass on the exceptions received at filter layer to the MVC layer global exception hanlder

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/security/JwtFilter.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/security/JwtFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -22,30 +23,40 @@ public class JwtFilter extends OncePerRequestFilter {
 
     private final UserRepository userRepository;
     private final AuthUtil authUtil;
+
+    //we use this to throws the exceptions recieved at the filter layer (Eg: JWT token expiry)
+    // to be handled by the global exception handler
+    private final HandlerExceptionResolver handlerExceptionResolver;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        log.info("incoming request: {} " , request.getRequestURI());
+        try {
 
-        final String requestTokenHeader = request.getHeader("Authorization");
 
-        if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer")){
-            filterChain.doFilter(request,response);
-            return;
+            log.info("incoming request: {} ", request.getRequestURI());
+
+            final String requestTokenHeader = request.getHeader("Authorization");
+
+            if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String token = requestTokenHeader.split("Bearer ")[1];
+            String username = authUtil.getUsernameFromToken(token);
+
+            log.info("incoming request username {}", username);
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                User user = userRepository.findByUsername(username).orElseThrow();
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+
+            filterChain.doFilter(request, response);
+        }catch (Exception exception){
+            handlerExceptionResolver.resolveException(request,response ,null , exception );
         }
-
-        String token = requestTokenHeader.split("Bearer ")[1];
-        String username = authUtil.getUsernameFromToken(token);
-
-        log.info("incoming request username {}" , username);
-
-        if (username !=null && SecurityContextHolder.getContext().getAuthentication()==null){
-            User user = userRepository.findByUsername(username).orElseThrow();
-
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(user ,null , user.getAuthorities() );
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-        }
-
-        filterChain.doFilter(request,response);
     }
 }
--- src/main/java/com/codingshuttle/youtube/hospitalManagement/security/WebSecurityConfig.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/security/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,18 +26,19 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity
-                .csrf(csrfConfig -> csrfConfig.disable())
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**" , "/auth/**" ).permitAll()
+            httpSecurity
+                    .csrf(csrfConfig -> csrfConfig.disable())
+                    .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authorizeHttpRequests(auth -> auth
+                                    .requestMatchers("/public/**", "/auth/**").permitAll()
 //                        .requestMatchers("/admin/**").hasRole("ADMIN")
 //                        .requestMatchers("/doctors/**").hasAnyRole("ADMIN", "DOCTOR")
-                                .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtFilter , UsernamePasswordAuthenticationFilter.class);
+                                    .anyRequest().authenticated()
+                    )
+                    .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 //                .formLogin(Customizer.withDefaults());
-        return httpSecurity.build();
+            return httpSecurity.build();
+
     }
 
 }
