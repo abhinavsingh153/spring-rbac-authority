633c61ad550acc7a8d2177af34045b72d27b880a
abhinav <abhinavsingh153@gmail.com>
2026-03-06 22:06:54 +0530

    add oauth2 dependency and google client id client secret

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/security/WebSecurityConfig.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/security/WebSecurityConfig.java
@@ -1,23 +1,31 @@
 package com.codingshuttle.youtube.hospitalManagement.security;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
+import java.io.IOException;
+
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class WebSecurityConfig {
 
     private final JwtFilter jwtFilter;
@@ -34,7 +42,12 @@ public class WebSecurityConfig {
 //                        .requestMatchers("/doctors/**").hasAnyRole("ADMIN", "DOCTOR")
                                     .anyRequest().authenticated()
                     )
-                    .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                    .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                    .oauth2Login(oauth2 -> oauth2.failureHandler(
+                            (request, response, exception) -> {
+                                log.error("Oauth2 error: {}" , exception.getMessage());
+                            }
+                    ));
 //                .formLogin(Customizer.withDefaults());
             return httpSecurity.build();
 
