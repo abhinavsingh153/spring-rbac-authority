ea801e68199cb49c067ab43032305fd11d0dfc2d
abhinav <abhinavsingh153@gmail.com>
2026-03-10 18:37:26 +0530

    Update HospitalManagementApplication.java

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/HospitalManagementApplication.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/HospitalManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HospitalManagementApplication {
 
+//    Hospital
     public static void main(String[] args) {
         SpringApplication.run(HospitalManagementApplication.class, args);
     }
